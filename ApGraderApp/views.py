4f516d8b5685a636f999bab5328e726110a4cde6
itscool2b <megagrb16@gmail.com>
2024-12-10 15:46:29 -0500

    ASYNC

--- ApGraderApp/views.py
+++ ApGraderApp/views.py
@@ -6,25 +6,26 @@ from PyPDF2 import PdfReader
 from .AI import evaluate_essay
 from asgiref.sync import sync_to_async
 import fitz
-
+import aiofiles
 import logging
 logger = logging.getLogger(__name__)
 
 @csrf_exempt
-def process(request):
+async def process(request):
     try:
         if request.method == "POST":
             if 'file' not in request.FILES:
                 return JsonResponse({'error': 'No file provided'}, status=400)
 
             pdf_file = request.FILES['file']
-            data = pdf_file.read()
+            async with aiofiles.open(pdf_file.temporary_file_path(), mode='rb') as f:
+                data = await f.read()
             # Ensure the file is a readable PDF
             doc = fitz.open(stream=data, filetype="pdf")
             student_essay = "".join([page.get_text() for page in doc])
             logger.info("Completed PDF text extraction")
             # Process the essay
-            response = evaluate_essay(student_essay)
+            response = await sync_to_async(evaluate_essay)(student_essay)
             return JsonResponse({'response': response}, status=200)
 
         return JsonResponse({'error': 'Invalid request method'}, status=405)
