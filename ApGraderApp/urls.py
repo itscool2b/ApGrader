a19438d074282c3a91563a0726e1665702612d06
itscool2b <megagrb16@gmail.com>
2024-12-17 16:22:47 -0500

    push

--- ApGraderApp/urls.py
+++ ApGraderApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import process
-from .views import process_prompt
+
 urlpatterns = [
-    path("essay", process, name='process'),
-    path("prompt", process_prompt, name='process_prompt')
+    path("process", process, name='process'),
+    
 ]
