61250d018a8407fdd7de574633529df18c9a0c39
Arjun Bajpai <itscool2b@Arjuns-Laptop.local>
2024-12-22 01:17:59 -0500

    im getting a new turbogit add .git add .xcode-select --install

--- ApGraderProj/settings.py
+++ ApGraderProj/settings.py
@@ -127,3 +127,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
