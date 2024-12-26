e0ccb4cfd09c1376bfab2b74468f1de133b4ccc4
Arjun Bajpai <itscool2b@Arjuns-Laptop.local>
2024-12-26 02:12:24 -0400

    j

--- ApGraderApp/urls.py
+++ ApGraderApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import ApushLEQ
+from .views import ApushLEQ, saq_view
 
 urlpatterns = [
     path("ApushLEQ", ApushLEQ, name='ApushLEQ'),
-    path("ApushSAQ", views.saq_view, name='ApushSAQ')
+    path("ApushSAQ", saq_view , name='ApushSAQ')
 ]
