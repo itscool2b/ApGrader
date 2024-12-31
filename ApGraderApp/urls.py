63d4cfc356a0bba1b71f9d7b7d7f45003c7c8678
itscool2b <megagrb16@gmail.com>
2024-12-31 00:48:47 -0500

    k

--- ApGraderApp/urls.py
+++ ApGraderApp/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("ApEuroDBQ", views.eurodbq, name='ApEuroDBQ'),
     path("ApushLEQ", views.ApushLEQ, name='ApushLEQ'),
     path("ApushSAQ", views.saq_view , name='ApushSAQ'),
     path("ApushDBQ", views.dbq_view, name='ApushDBQ'),
     path("ApEuroLEQ", views.ApEuroLEQ, name='ApEuroLEQ'),
     path("ApEuroSAQ", views.eurosaq_view, name='ApEuroSAQ'),
-    path("ApEuroDBQ", views.eurodbq, name='ApEuroDBQ')
+    
 ]
